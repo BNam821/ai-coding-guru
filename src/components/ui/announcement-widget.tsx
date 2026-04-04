262fefaa1898437a7429c31fa531aff4ab1b803d
ZOXpex1309 <zetox21minecraft@gmail.com>
2026-04-04 16:42:58 +0700

    announcement8

--- src/components/ui/announcement-widget.tsx
+++ src/components/ui/announcement-widget.tsx
@@ -152,7 +152,7 @@ export function AnnouncementWidget() {
                         alt="Logo thông báo"
                         width={34}
                         height={34}
-                        className="h-10 w-10 object-contain"
+                        className="h-15 w-15 object-contain"
                         onError={() => setLogoSrc("/real_logo.png")}
                     />
                 </span>
